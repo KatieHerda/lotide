b07414f4449caf08622a8e9e66c611a8a4b03f59
Katie <katie.herda2@gmail.com>
2021-09-13 22:03:11 +0000

    Using console.assert to check if statements are true or false.

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🛑🛑🛑 Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`✅✅✅  Assertion failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("hello", "hello");
+assertEqual(0, false);
