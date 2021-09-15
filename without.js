755d757fc3ab453c1768d20a077191c565ce4e52
Katie <katie.herda2@gmail.com>
2021-09-15 15:55:48 +0000

    Funtion that will return a subset of a given array, removing unwanted elements. First commit.

---
+++ without.js
@@ -0,0 +1,42 @@
+const eqArrays = function(arrayOne, arrayTwo) {
+  //If length of both arrays is not equal, then you don't even have to pusue anything further.
+  if (arrayOne.length !== arrayTwo.length) {
+    return false;
+  }
+  //compare values in each array[i] to see if they are NOT equal.
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i]) {
+      return false; //If above cases return false, arrays are equal, return true.
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertation Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertation Failed: ${actual} !== ${expected}`);
+  }
+};
+
+//Implement without which will return a subset of a given array, removing unwanted elements.
+const without = function(sourceArray, itemsToRemove) {
+
+  let returnArray = [...sourceArray]; //hold final array
+
+  for (let i = 0; i < itemsToRemove.length; i++) { //loop through source array.
+    returnArray = (sourceArray.filter(function(ele) { 
+      return ele !== itemsToRemove[i]; //return elements to returnArray that are not filtered out (that match type & value of itemsToRemove)
+    }));
+  }
+  return returnArray;
+};
+
+console.log(without([1, 2, 3], [1])); // => [2, 3]
+console.log(without(["1", "2", "3"], [1, 2, "3"])); // => ["1", "2"]
+
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
