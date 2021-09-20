293b580bea2a143ed7b66741311b5d14dc689a91
Katie <katie.herda2@gmail.com>
2021-09-20 23:21:38 +0000

    removed tests that were meant to fail.

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,6 +1,5 @@
 const assertEqual = require('../assertEqual');
 
-assertEqual("Lighthouse Labs", "Bootcamp"); // => fail
+
 assertEqual(1, 1); // => pass
 assertEqual("hello", "hello"); // => pass
-assertEqual(0, false); // => fail
